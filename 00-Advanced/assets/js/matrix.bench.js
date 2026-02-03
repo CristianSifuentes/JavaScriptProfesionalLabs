e772a4d95f1ffd55dc5e47c9463d60c5860cb825
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-02-03 17:06:33 -0600

    Adding advance code for learn deep javascript

--- 00-Advanced/assets/js/matrix.bench.js
+++ 00-Advanced/assets/js/matrix.bench.js
@@ -3,11 +3,20 @@
 
 const os = require('perf_hooks');
 const { 
-    
+   Matrix,
+   mulConcurrent,
+   fillDeterministic,
+   withBlockRows,
+   withBlockRows
  } = require('./matrix');
 
- async function bench(params) {
-    
+ async function bench(name, fn, iters = 5) {
+    // Warm up
+    await fn();
+    const start = performance.now();
+    for(let i= 0; i < iters; i++) await fn();
+
+
  }
 
  (async() => {
